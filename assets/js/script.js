8783972b8eafa13eedf5dbcb19d0d380796d0fa2
Spare <t.spare.jkpg@gmail.com>
2023-03-20 13:34:43 +0000

    choose username prompt when pressing new game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let buttons = document.getElementsByTagName("button", "Image");
-
-    for let (button of buttons){
-        button.addEventListener("click", function(){
-            if (this.getAttribute("data-type") === ("New Game")
-                alert("LetsPLay!");
-    }           else{
-            let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-            } )
-        }})
+    const newGameButton = document.getElementById("newgame");
+    const someString = "Username";
+    
+    newGameButton.addEventListener(
+      "click",
+      function () {
+       sign = window.prompt("Choose a username to play");
+        console.log(sign);  // Expected Value: 'Username'
+      }.bind(someString)
+    );
+})
 
 let hairColorBlond = ("blond");         /* hair color variables */
 let hairColorBlack = ("black");
@@ -30,27 +30,21 @@ let ageOld = ("old");
 let male = ("male");
 let female = ("female");
 
-const c1 = 1, hairColorBlond, male, moodHappy;        /*- designate card values, from left to right in play area */
-const c2 = 2, hairColorBlond, female, moodNotHappy;
-const c3 = 3, hairColorBlack, female, moodNotHappy;
-const c4 = 4, hairColorBrwn, female, moodNotHappy;
-const c5 = 5, hairColorBlond, female, moodNotHappy;
-const c6 = 6, hairColorBlack, male, moodHappy;
-const c7 = 7, hairColorRed, female, moodHappy;
-const c8 = 8, hairColorBlond, male, moodNotHappy;
-const c9 = 9, hairColorBlack, male, moodNotHappy;
-const c10 = 10, hairColorBrwn, male, moodHappy;
-const c11 = 11, hairColorBlond, male, moodHappy;
-const c12 = 12, hairColorBrwn, female, moodNotHappy;
-const c13 = 13, hairColorBlond, male, moodNotHappy;
-const c14 = 14, hairColorBlond, male, moodHappy;    /* CPUs card */ 
+const c1 = (1, hairColorBlond, male, moodHappy);        /*- designate card values, from left to right in play area */
+const c2 = (2, hairColorBlond, female, moodNotHappy);
+const c3 = (3, hairColorBlack, female, moodNotHappy);
+const c4 = (4, hairColorBrwn, female, moodNotHappy);
+const c5 = (5, hairColorBlond, female, moodNotHappy);
+const c6 = (6, hairColorBlack, male, moodHappy);
+const c7 = (7, hairColorRed, female, moodHappy);
+const c8 = (8, hairColorBlond, male, moodNotHappy);
+const c9 = (9, hairColorBlack, male, moodNotHappy);
+const c10 = (10, hairColorBrwn, male, moodHappy);
+const c11 = (11, hairColorBlond, male, moodHappy);
+const c12 = (12, hairColorBrwn, female, moodNotHappy);
+const c13 = (13, hairColorBlond, male, moodNotHappy);
+const c14 = (14, hairColorBlond, male, moodHappy);    /* CPUs card */ 
 
-
-
-
-
-
-  
 
 
 
