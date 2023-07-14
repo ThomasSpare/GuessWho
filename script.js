83850d8a90844cd883f06d29cce4023888cb6165
Spare <t.spare.jkpg@gmail.com>
2023-07-14 21:01:56 +0000

    add classlist to StripImg function to fade down when cards are asked for attributes

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  //userInput = document.getElementById('userInput')
-  //userInput.addEventListener('input', (e) => {
 
-    })
-    const newGameButton = document.getElementById("newgamebtn")              // At load of page player will get prompted to enter a username
-    const someString = "Username";                                         // If blank alert enter username
+const newGameButton = document.getElementById("newgamebtn")              // At load of page player will get prompted to enter a username
+const someString = "Username";                                         // If blank alert enter username
 
 newGameButton.addEventListener("click", function(){
   var userName = document.getElementById("myText").value;             // Username input, display instructions when press play
@@ -15,8 +11,6 @@ newGameButton.addEventListener("click", function(){
       document.getElementById("letsplay").innerHTML = "Lets play " + userName +" !";
       document.getElementById("instructions").innerHTML ="    The goal of the game is to guess who the person is behind the hidden card.<br>    You only have One guess. Choose a question and hit ASK to get more clues";
 });
-
-
 
 // This Array consist of all the people on the Cards with 7 properties (and one img property) and values
 
@@ -52,33 +46,6 @@ const people = [ {hair: 'blond', hairtype: 'short',sex:'man', mood:'angry', glas
               {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: 'assets/images/characters/30.jpg'}
             ]
 
-
-
-
-document.addEventListener('change', (e) => {
-
-   const question = e;
-});
-
-
-// let sexVal = document.querySelector('input[name="sex"]:checked').value
-//   if sexVal = 'checked'
-// then let sexVal =  
-
-
-
-let sexVal = document.querySelector('input[name="sex"]:checked')         // Input of property from radio buttons 
-let hairVal = document.querySelector('input[name="hair"]:checked')
-let hairtypeVal = document.querySelector('input[name="hairtype"]:checked')
-let moodVal = document.querySelector('input[name="mood"]:checked')
-let ageVal = document.querySelector('input[name="age"]:checked')
-let glassesVal = document.querySelector('input[name="glasses"]:checked')
-let beardVal = document.querySelector('input[name="beard"]:checked')
-
-function Question(){
-}
-
-
 hiddenpeople =['1.jpg', '2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg',
                '16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg','22.jpg','23.jpg','24.jpg','25.jpg','26.jpg','27.jpg','28.jpg','29.jpg','30.jpg']
 
@@ -90,11 +57,12 @@ hiddenpeople =['1.jpg', '2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg',
 // and its value and id 
 
 const radioChecked = e => {
+  
   let radioButtons = document.getElementsByName('radio');
   for (let radio of radioButtons) {
      if (radio.checked) {
-      document.getElementById("instructions").innerHTML = "The radio button is selected and it's value is " + radio.value + " and its id is " + radio.id;
-      console.log("A radio button is selected and it's value is " + radio.value + " and its id is " + radio.id);
+      document.getElementById("instructions").innerHTML = "You asked if " + radio.id + " is " + radio.value;
+      console.log("You asked if " + radio.id + " is " + radio.value);
       let questionProp = radio.id;
       let questionVal = radio.value;
       const filteredPeople = filterPeople(people, {
@@ -106,11 +74,10 @@ const radioChecked = e => {
       const props = Object.keys(person);
         return props[index] === 'img';
       });
-      console.log(ValuesWithoutImg);
+      ValuesWithoutImg.forEach(element => StripImg(element));   // Function executes a provided function once for each array element
     });
 }};
 }
-
 
 
 // FilterFunction with filters{ questionProp: questionVal }
@@ -124,26 +91,43 @@ function filterPeople(people, filters) {
 }
 
 
+// ----- On new game button click random hidden Guess Who card is generated
 
-
-
-
-
-
-
-
-
-// -----------------
-
-function renderHiddenImage(){                                      // On new game button click random hidden card is generated
+function renderHiddenImage(){                                      
   random_index = Math.floor(Math.random() * hiddenpeople.length);
   selected_image = hiddenpeople[random_index]
   let person = document.getElementById('slot1')
   let hiddencard = document.getElementById('slot1').src =`/assets/images/characters/${selected_image}`
   hiddencard = (getFilename(hiddencard));
   console.log(hiddencard);
-    return hiddencard;
+  return hiddencard, person;
+
 }
+
+
+function StripImg(fullPath) {                       //  Returns the array ValuesWithoutImg as only ID numbers (eg. 3, 12, 29 ) 
+  let ValueId = fullPath.replace(/^.*[\\\/]/, '');
+  ValueId = removeExtension(ValueId);
+  if (ValueId != null){
+    const MyList = document.getElementById("card" + ValueId );    // str "card" is added for each id passed and added to classList 
+    MyList.classList.toogle('fade'); 
+    console.log(MyList.classList);                                 // Each card is added the class fade which dims brightness
+  }
+  else{
+    // do nothing
+  }                                          // Add style to selected Cards
+}
+
+
+function changeStyle(){  
+  const element = document.querySelector("#slot1");    // Makes hidden card invisible when pushing new game button
+  var sheet = document.createElement('style')
+  sheet.innerHTML = "#slot1{filter: brightness(0%);}";
+  document.body.appendChild(sheet);
+}
+
+
+// ----  Utils  ---------------
 
 function getFilename(fullPath) {                    //  Returns the img filename without the path 
   return fullPath.replace(/^.*[\\\/]/, '');
@@ -151,19 +135,16 @@ function getFilename(fullPath) {                    //  Returns the img filename
 
 
 
-function changeStyle(){                             // Makes hidden card invisible when pushing new game button
-  var element = document.getElementById("");
-  var sheet = document.createElement('style')
-  sheet.innerHTML = "#slot1{filter: brightness(0%);}";
-  document.body.appendChild(sheet);
+function addSpace(str) {                // adds a space between valueId as this is needed to use with innerHTML 
+  return str.split('').join(' ');
 }
 
-function fadeStyle(){                          // Makes cards with selected features fade when pushing ask button
-  var element = document.getElementsByName('slot');
-  var sheet = document.createElement('style')
-  sheet.innerHTML = "#slot{filter: brightness(20%);}";
-  document.body.appendChild(sheet);
+function removeExtension(filename) {
+  return filename.substring(0, filename.lastIndexOf('.')) || filename;
+
 }
+
+
 
 // USERGUESS AND HIDDENCARD FUNCTIONS
 // When user click on boardcards the selected cards name is returned as userguess (*.jpg)
@@ -198,16 +179,23 @@ gueessBtn.addEventListener('click', function(){
 }
 })
 
-
+if (newGameButton, "click"){
 function renderBoardImages(Array){
    for (var i = 0; i <= 29; ++i){
    index = (29 - i);
    selected_image = hiddenpeople[index];
-   document.getElementById(index).src =`/assets/images/characters/${selected_image}`
- }}
+   document.getElementById("card" + index).src =`/assets/images/characters/${selected_image}`
+ }}}
+ else{
+  // do nothing
+ }
 
+if (newGameButton, "click"){
 renderBoardImages(hiddenpeople);
-
+}
+else{
+  document.getElementById("instructions").innerHTML = "You asked if " + radio.id + " is " + radio.value;
+}
 
 
 function guessPerson(userguess, hiddencard){
@@ -219,43 +207,3 @@ function guessPerson(userguess, hiddencard){
     document.getElementById("guess").innerHTML ='GAME OVER';
   }
 }
-
-
-
-function ask(){
-
-  document.getElementsByClassName("DIALOG").innerHTML = "Good, now just hit ask";
-}
-
-
-
-
-
-
-
-function releaseNameButton(){
-letVal = document.querySelector();
-  console.log(letVal);
-}
-
-
-
-
-function startGame() {
-  
-  // shuffle people to slots
-  renderImages(people);
-
-}
-
-
-
-// print img to slots
-function renderImages() {
-  for (let p of people) {
-  let img = document.createElement(img)
-  body.append(img)
-  document.getElementById('slot1').innerHTML = (img);
-}
-}
-
