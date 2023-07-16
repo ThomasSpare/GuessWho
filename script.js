99ed89250096c7fdf221459b8caa3065806f46a2
Spare <t.spare.jkpg@gmail.com>
2023-07-16 10:24:45 +0000

    add ./assets/

--- script.js
+++ script.js
@@ -16,36 +16,36 @@ newGameButton.addEventListener("click", function(){
 
 // This Array consist of all the people on the Cards with 7 properties (and one img property) and values
 
-const people = [ {hair: 'blond', hairtype: 'short',sex:'man', mood:'angry', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/1.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/2.jpg'},
-              {hair: 'black', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/3.jpg'},
-              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/4.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/5.jpg'},
-              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/6.jpg'},
-              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/7.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/8.jpg'},
-              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/9.jpg'},
-              {hair: 'brown', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/10.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/11.jpg'},
-              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/12.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/13.jpg'},
-              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/14.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/15.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/16.jpg'},
-              {hair: 'white', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/17.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'woman', mood: 'happy', glasses: 'yes', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/18.jpg'},
-              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'yes', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/19.jpg'},
-              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'yes', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/20.jpg'},
-              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/21.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/22.jpg'},
-              {hair: 'no', hairtype: 'no', sex: 'man', mood: 'neutral', glasses: 'no', age:'yes', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/23.jpg'},
-              {hair: 'no', hairtype: 'no', sex: 'man', mood: 'neutral', glasses: 'no', age:'yes', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/24.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/25.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/26.jpg'},
-              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/27.jpg'},
-              {hair: 'brown', hairtype: 'short', sex: 'man', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/28.jpg'},
-              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/29.jpg'},
-              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: 'https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/30.jpg'}
+const people = [ {hair: 'blond', hairtype: 'short',sex:'man', mood:'angry', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/1.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/2.jpg'},
+              {hair: 'black', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/3.jpg'},
+              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/4.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/5.jpg'},
+              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/6.jpg'},
+              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/7.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/8.jpg'},
+              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/9.jpg'},
+              {hair: 'brown', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/10.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/11.jpg'},
+              {hair: 'red', hairtype: 'long', sex: 'woman', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/12.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/13.jpg'},
+              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/14.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral',glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/15.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/16.jpg'},
+              {hair: 'white', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/17.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'woman', mood: 'happy', glasses: 'yes', age:'no', beard: 'no', img: './assets/images/characters/18.jpg'},
+              {hair: 'black', hairtype: 'short', sex: 'man', mood: 'happy', glasses: 'yes', age:'no', beard: 'no', img: './assets/images/characters/19.jpg'},
+              {hair: 'blond', hairtype: 'short', sex: 'man', mood: 'neutral', glasses: 'no', age:'no', beard: 'yes', img: './assets/images/characters/20.jpg'},
+              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/21.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/22.jpg'},
+              {hair: 'no', hairtype: 'no', sex: 'man', mood: 'neutral', glasses: 'no', age:'yes', beard: 'no', img: './assets/images/characters/23.jpg'},
+              {hair: 'no', hairtype: 'no', sex: 'man', mood: 'neutral', glasses: 'no', age:'yes', beard: 'no', img: './assets/images/characters/24.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/25.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'man', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/26.jpg'},
+              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'neutral', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/27.jpg'},
+              {hair: 'brown', hairtype: 'short', sex: 'man', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/28.jpg'},
+              {hair: 'brown', hairtype: 'long', sex: 'woman', mood: 'happy', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/29.jpg'},
+              {hair: 'blond', hairtype: 'long', sex: 'woman', mood: 'angry', glasses: 'no', age:'no', beard: 'no', img: './assets/images/characters/30.jpg'}
             ]
             https://github.com/ThomasSpare/GuessWho/blob/Main
 hiddenpeople =['1.jpg', '2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg',
@@ -97,7 +97,7 @@ function renderHiddenImage(){
   random_index = Math.floor(Math.random() * hiddenpeople.length);
   selected_image = hiddenpeople[random_index]
   let person = document.getElementById('slot1')
-  let hiddencard = document.getElementById('slot1').src =`https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/${selected_image}`
+  let hiddencard = document.getElementById('slot1').src =`./assets/images/characters/${selected_image}`
   hiddencard = (getFilename(hiddencard));
   return hiddencard, person;
 
@@ -181,7 +181,7 @@ function renderBoardImages(Array){
    for (var i = 1; i <= 29; ++i){
    index = (30 - i);
    selected_image = hiddenpeople[index];
-   document.getElementById("card" + (index + 1)).src =`https://github.com/ThomasSpare/GuessWho/blob/Guess-who-version-2/assets/images/characters/${selected_image}`
+   document.getElementById("card" + (index + 1)).src =`./assets/images/characters/${selected_image}`
  }}}
  else{
   // do nothing
