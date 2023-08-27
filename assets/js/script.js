f879515c530f884c3d320f0f687cd086b5cdffcf
Spare <t.spare.jkpg@gmail.com>
2023-08-27 10:29:56 +0000

    Add resetGame function, StripImg correct features now dont fade all but the hiddencard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,35 @@
-import { people } from "../js/people.js"
-import { hiddenpeople } from "../js/hiddenpeople.js"
+import { people } from "../js/people.js";
+import { hiddenpeople } from "../js/hiddenpeople.js";
 
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
+  const newGameButton = document.getElementById("newgamebtn");
+  const askBtn = document.getElementById("askBtn");
+  const guessBtn = document.getElementById("guess");
+  const resetBtn = document.getElementById("resetBtn");
 
-});
+  let someString = "Username";
 
-const newGameButton = document.getElementById("newgamebtn")        // At load of page player will get prompted to enter a username
-let someString = "Username";                                     // If blank alert enter username
+  newGameButton.addEventListener("click", function() {
+    var userName = document.getElementById("myText").value;
+    if (userName === "") {
+      alert('Please enter a username');
+    } else {
+      document.getElementById("letsplay").innerHTML = "Lets play " + userName + " !";
+      document.getElementById("instructions").innerHTML = "The goal of the game is to guess who the person is behind the hidden card.<br>You only have One guess. Choose a question and hit ASK to get more clues";
+      document.getElementById("newgamebtn").innerHTML = "Click to start !";
+      newGameButton.addEventListener("click", renderHiddenImage);
+      newGameButton.addEventListener("click", renderBoardImages);
+      newGameButton.addEventListener("click", changeStyle);
+      resetBtn.addEventListener("click", resetCards);
+      askBtn.addEventListener("click", radioChecked);
+      guessBtn.addEventListener("click", guessPerson);
+      return userName}
+  });
+};
 
-newGameButton.addEventListener("click", function(){
-var userName = document.getElementById("myText").value;         // Username input, display instructions when press play
-if (userName === ""){
-alert('Please enter a username');
-} 
-  document.getElementById("letsplay").innerHTML = "Lets play " + userName +" !";
-  document.getElementById("instructions").innerHTML ="    The goal of the game is to guess who the person is behind the hidden card.<br>    You only have One guess. Choose a question and hit ASK to get more clues";
-});
-
-
-newGameButton("click", renderHiddenImage, renderBoardImages, changeStyle);
-
-//----------  RADIOBUTTONS
-
-// Function that checks what button was selected when user push ask button 
-// and its value and id ..that is ( value and prop )
+/* RADIOBUTTONS
+    Function that checks what button was selected when user push ask button 
+    and its value and id ..that is ( value and prop ) */
 
 let radioChecked = e => {
 let radioButtons = document.getElementsByName('radio');
@@ -41,13 +47,14 @@ filteredPeople.forEach(person => {
   const props = Object.keys(person);
     return props[index] === 'img';
   });
-  ValuesWithoutImg.forEach(element => StripImg(element));   // Function executes a provided function once for each array element
+// Function executes a provided function once for each array element
+  ValuesWithoutImg.forEach(element => StripImg(element));   
 });
 }};
 }
 
 
-// FilterFunction with filters{ questionProp: questionVal }
+// FILTERFUNCTION with filters{ questionProp: questionVal }
 
 function filterPeople(people, filters) {
 return people.filter(person => {
@@ -67,17 +74,18 @@ return renderHiddenImage, hiddencard, person;
 }
 
 
-// ----- On new game button click random hidden Guess Who card is generated
+// On new game button click random hidden Guess Who card is generated
 var hiddencard = renderHiddenImage();
 var userguess = userguess;
 
+/* GUESS FUNCTION The player first clicks on the person he/she thinks it is
+    Then clicks the button to confirm if the guess is correct
+    The result win or lose is declared and the hiddencard is displayed to the user */
 function guessPerson(){                                           
 document.getElementById("guess").innerHTML ='Click on who you think it is';
 var img_src = document.getElementById("slot1").src;
 img_src = ("card" + getFilename(img_src));
-                    // Guess function: The player first clicks on the person he/she thinks it is
-                    // Then clicks the button to confirm if the guess is correct
-if (userguess.toString() === img_src.toString())    // The result win or lose is declared and the hiddencard is displayed to the user
+if (userguess.toString() === img_src.toString())    
 {
 document.getElementById("guess").innerHTML ='Its correct ! You win !';
 var sheet = document.createElement('style')
@@ -90,71 +98,82 @@ document.getElementById("guess").innerHTML ='Sorry wrong guess ! You lose !';
 var sheet = document.createElement('style')
 sheet.innerHTML = "#slot1{filter: brightness(100%);}";
 document.body.appendChild(sheet);
-}
-}
+}}
 
-
-
-
+// Returns the array ValuesWithoutImg as only ID numbers (eg. 3, 12, 29 )
 function StripImg(fullPath) {
 var img_src = document.getElementById("slot1").src;
 img_src = ("card" + getFilename(img_src));
 let ValueId = fullPath.replace(/^.*[\\\/]/, '');
-ValueId = removeExtension(ValueId);                       //  Returns the array ValuesWithoutImg as only ID numbers (eg. 3, 12, 29 )
+ValueId = removeExtension(ValueId);                       
 if (("card" + ValueId + ".jpg").toString() === (img_src.toString()))
 {
 document.getElementById("instructions").innerHTML = "The Hidden Card has this Attribute !";
-return img_src;
+MyList.classList.remove('fade');
+MyList.classList.add('slot');
+return img_src, ValueId;
 }
 else
+// str "card" is added for each id passed and added to classList 
 {  
 if (ValueId <= 30){
-  const MyList = document.getElementById("card" + ValueId );    // str "card" is added for each id passed and added to classList 
+  const MyList = document.getElementById("card" + ValueId );    
   MyList.classList.remove('slot');
   MyList.classList.add('fade');
+  return ValueId;
 }
 else{
-  // do nothing
+// do nothing
 }                                        
 }}
 
+// RESET GAME FUNCTION activated by Play Again Button
+function resetCards(){
+  document.getElementById("instructions").innerHTML = "Lets Play a New game !";
+  renderHiddenImage();
+  changeStyle();
+  document.getElementById("guess").innerHTML ='GUESS WHO !';
+  for (var i = 1; i <= 29; ++i){
+    let index = (31 - i);
+  const MyList2 = document.getElementById("card" + index);
+  MyList2.classList.remove('fade');
+  MyList2.classList.add('slot');
+}}
+
+// Makes hidden card blacked out when pushing new game button
 function changeStyle(){  
-const element = document.querySelector("#slot1");    // Makes hidden card blacked out when pushing new game button
+const element = document.querySelector("slot1");
 var sheet = document.createElement('style')
 sheet.innerHTML = "#slot1{filter: brightness(0%);}";
 document.body.appendChild(sheet);
 }
 
+// ----  Utils SMALL FUNCTIONS---------------
+//  Returns the img filename without the path
 
-// ----  Utils  ---------------
-
-function getFilename(fullPath) {              //  Returns the img filename without the path 
-return fullPath.replace(/^.*[\\\/]/, '');
+function fadeDiv(){
+  const playList = document.getElementById("play_game");
+  playList.classList.remove("lets_play");
+  playList.classList.add("lets_play_fade");
 }
 
-
-
-function addSpace(str) {                // adds a space between valueId as this is needed to use with some innerHTML 
-return str.split('').join(' ');
+function getFilename(fullPath) {
+return fullPath.replace(/^.*[\\\/]/, '');
 }
 
 function removeExtension(filename) {
 return filename.substring(0, filename.lastIndexOf('.')) || filename;
-
 }
 
-// USERGUESS AND HIDDENCARD FUNCTIONS
-// When user click on boardcards the selected cards name is returned as userguess (*.jpg)
-// This lets us compare to the hiddencard (x.jpg)
+/* USERGUESS AND HIDDENCARD FUNCTIONS
+    When user click on boardcards the selected cards name is returned as userguess (*.jpg)
+    This lets us compare to the hiddencard (x.jpg) */
 
-// CLICKED CARD
 var images = document.querySelectorAll('img');
 
 let imgPressed = e => {
 userguess = (e.target.id + ".jpg")
-console.log(userguess);
 return userguess;
-// This is console logged to show clicked cards id
 // The card the user clicks before pushing the guess button
 }
 
@@ -163,19 +182,15 @@ image.addEventListener("click", imgPressed);
 }
 var images = document.querySelectorAll('img');
 
+/* RENDERBOARD CARDS renders the cards to slot 1-30 */
 function renderBoardImages(Array){
-for (var i = 1; i <= 29; ++i){
-let index = (30 - i);
+fadeDiv();
+for (var i = 0; i <= 29; ++i){
+let index = (29 - i);
 let /*  */selected_image = hiddenpeople[index];
 document.getElementById("card" + (index + 1)).src =`./assets/images/characters/${selected_image}`
 }}
 
-if (newGameButton, "click"){
-renderBoardImages(hiddenpeople);
-}
-else{
-
-}
 
 function innerHtml(){
 document.getElementById("instructions").innerHTML = "You asked if " + radio.id + " is " + radio.value;
